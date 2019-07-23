7b11a123d554758a6305d0587c80a1d239057e01
wangyuanzhengbighead <zjajwyz@163.com>
2019-07-23 14:01:00 +0800

    提交修改，提交删除倒数第n个节点删除方案

--- list/reverseBetween.c
+++ list/reverseBetween.c
@@ -8,23 +8,23 @@
 
 /* 从m到n翻转链表 */
 struct ListNode* reverseBetween(struct ListNode* head, int m, int n){
-	struct ListNode *p, *q, *new;
+	struct ListNode *p, *q;
 	struct ListNode HEAD;
 	int index, num = m;
-	
-	HEAD.next = head;
 
 	if(num >= n) {
 		return head;
 	}
 
+	HEAD.next = head;
+	head = &HEAD;
+
 	/* 保存第一个前驱节点 */
-	new = &HEAD;
-	while(new && num>1) {
-		new = new->next;
+	while(head && num>1) {
+		head = head->next;
 		num--;
 	}
-	p = new->next;
+	p = head->next;
 
 	/* 翻转[m, n]区间链表 */
 	index = 0;
@@ -32,8 +32,8 @@ struct ListNode* reverseBetween(struct ListNode* head, int m, int n){
 	{
 		q = p->next;
 		p->next = q->next;
-		q->next = new->next;
-		new->next = q;
+		q->next = head->next;
+		head->next = q;
 		index++;
 	}
 
